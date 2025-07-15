3144800a617e9ab420975ae96dad964a2b179ca5
sumitti <st023429@gmail.com>
2025-07-15 19:48:16 +0530

    Modify file

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,19 +1,15 @@
-import express from 'express';
-import { login, logout, signup , updateProfile, checkAuth} from '../controller/auth.controller.js';
-import { protectRoute } from '../middleware/auth.middleware.js';
+import express from "express";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
+router.put("/update-profile", protectRoute, updateProfile);
 
-router.post('/signup', signup);
-
-router.post('/login', login);
-
-router.post('/logout', logout);
-
-router.put('/update-profile', protectRoute, updateProfile);
-
-router.get('/check', protectRoute, checkAuth);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;

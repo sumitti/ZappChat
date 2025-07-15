4c601117de49bc91770659312c6dedf951e0fad3
sumitti <st023429@gmail.com>
2025-07-15 18:27:32 +0530

    Modify index file

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.NODE_ENV === "production"
+    ? "https://zappchat-1z4w.onrender.com"
+    : "http://localhost:5173",
     credentials: true,
   })
 );

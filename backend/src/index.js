a669a5c74c56acc6e131d435216815d2d5e2ecf2
sumitti <st023429@gmail.com>
2025-07-15 18:01:21 +0530

    Fix the routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: 'http://localhost:5173', 
     credentials: true,
 }));
 

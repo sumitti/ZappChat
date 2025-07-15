204bb29f36ddf18138976b09717c15f2131c995e
sumitti <st023429@gmail.com>
2025-07-15 20:40:35 +0530

    Modify file

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,9 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production"
-    ? "https://zappchat-1z4w.onrender.com"
-    : "http://localhost:5173",
+    origin:"http://localhost:5173",
     credentials: true,
   })
 );
@@ -33,9 +31,9 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+//   app.get("/*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
 }
 
 server.listen(PORT, () => {
